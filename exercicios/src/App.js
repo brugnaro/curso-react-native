d0cca330776d982180534d2b4b1bfff34b1a27a8
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-09-06 12:02:35 -0300

    training import and exports

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import Simples from "./components/Simples";
 import ParImpar from "./components/ParImpar";
+import { Inverter, MegaSena } from "./components/Multi";
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,8 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Simples text="Flexível!!" />
         <ParImpar numero={31} />
+        <Inverter texto="marcos!" />
+        <MegaSena numeros={9} />
       </View>
     );
   }
