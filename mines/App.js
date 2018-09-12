d87d8f594836f1b07a6dab6f06968ba6fddd6c11
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-09-11 22:23:30 -0300

    Begin field.

--- mines/App.js
+++ mines/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import params from "./src/params";
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Inicial</Text>
         <Text>
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
     margin: 10
   }
-});
+})
